5f2ec6a5635b9f10a6891e86cbb154d8f2f31a02
Guto Xavier <gutoffline@gmail.com>
2019-01-23 21:16:36 -0200

    Arquivos para começar os trabalhos

---
+++ 2019/Janeiro/CalaboucosEDragoes/CalaboucosEDragoes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalaboucosEDragoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome="", classe="", raca = "";
+            int vida = 100;
+
+            Console.WriteLine("Informe seu nome aventureiro.");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua raça.");
+            raca = Console.ReadLine();
+
+            Console.WriteLine("Informe sua classe.");
+            classe = Console.ReadLine();
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Ficha: ");
+            Console.WriteLine(nome);
+            Console.WriteLine(classe);
+            Console.WriteLine(raca);
+            Console.WriteLine("---------------------------------");
+
+            string resposta = "";
+            Console.WriteLine("Deseja entrar na porta a sua direita?");
+            resposta = Console.ReadLine();
+
+            if(resposta == "sim")
+            {
+
+
+            }
+        }
+    }
+}
---
+++ 2019/Janeiro/Exercicio15/Exercicio15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use a estrutura de repetição enquanto condição faz para mostrar uma contagem de números pares na tela.Essa contagem deve iniciar em 0 e terminar com um número previamente informado pelo usuário.
+
+            int numero = 0;
+            int contador = 0;
+            Console.WriteLine("Informe um número")
+            numero = Int32(Console.ReadLine());
+            if(numero % 2 == 0)
+            {
+                Console.WritLine("O número que você digitou é par. Vamos usar ele.");
+            }
+            else
+            {
+                numero++;
+                Consloe.WriteLine("O número que você digitou é par. Vamos usar ele esse " +numero + ".");
+            }
+            for(contador = 0; contdor <= numreo ; contador = contador + 2)
+            {
+                Console.WriteLine(contador);
+            }
+            Console.ReadKey();
+        }
+    }
+}
