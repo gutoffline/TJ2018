ab7bb24ca7f667a76fd20c803185c7c085d989cf
Guto Xavier <gutoffline@gmail.com>
2019-01-23 22:14:53 -0200

    Exercícios resolvidos

--- 2019/Janeiro/Exercicio15/Exercicio15/Program.cs
+++ 2019/Janeiro/Exercicio15/Exercicio15/Program.cs
@@ -10,26 +10,29 @@ namespace Exercicio15
     {
         static void Main(string[] args)
         {
-            //Use a estrutura de repetição enquanto condição faz para mostrar uma contagem de números pares na tela.Essa contagem deve iniciar em 0 e terminar com um número previamente informado pelo usuário.
+        //Use a estrutura de repetição enquanto condição faz para mostrar uma contagem de números pares na tela.Essa contagem deve iniciar em 0 e terminar com um número previamente informado pelo usuário.
 
-            int numero = 0;
-            int contador = 0;
-            Console.WriteLine("Informe um número")
-            numero = Int32(Console.ReadLine());
-            if(numero % 2 == 0)
-            {
-                Console.WritLine("O número que você digitou é par. Vamos usar ele.");
-            }
-            else
-            {
-                numero++;
-                Consloe.WriteLine("O número que você digitou é par. Vamos usar ele esse " +numero + ".");
-            }
-            for(contador = 0; contdor <= numreo ; contador = contador + 2)
-            {
-                Console.WriteLine(contador);
-            }
-            Console.ReadKey();
+        int numero = 0;
+        int contador = 0;
+        Console.WriteLine("Informe um número");
+        numero = Int32.Parse(Console.ReadLine());
+
+        if(numero % 2 == 0)
+        {
+            Console.WriteLine("O número que você digitou é par. Vamos usar ele.");
+        }
+        else
+        {
+        numero++;
+            Console.WriteLine("O número que você digitou é ímpar. Vamos usar ele esse " +numero + ".");
+        }
+
+        for(contador = 0; contador <= numero ; contador = contador + 2)
+        {
+            Console.WriteLine(contador);
+        }
+
+        Console.ReadKey();
         }
     }
 }
