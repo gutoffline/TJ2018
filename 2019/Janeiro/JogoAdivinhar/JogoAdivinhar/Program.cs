4bc4990e58a197dcb06562ccc92e64029fa4d90a
Guto Xavier <gutoffline@gmail.com>
2019-01-30 20:59:57 -0200

    Jogos de Advinhação e Par ou Ímpar

---
+++ 2019/Janeiro/JogoAdivinhar/JogoAdivinhar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoAdivinhar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUsuario, numeroAleatorio;
+
+            Random aleatorio = new Random();
+            numeroAleatorio = aleatorio.Next(0, 10);
+
+            Console.WriteLine("Informe um número de 0 à 10");
+            numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            if(numeroUsuario == numeroAleatorio)
+            {
+                Console.WriteLine("Parabéns, você ganhou o jogo!");
+            }
+            else
+            {
+                Console.WriteLine("Que pena! Você perdeu.");
+            }
+
+            Console.WriteLine("Número sorteado " + numeroAleatorio);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2019/Janeiro/JogoParImpar/JogoParImpar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroAleatorio, numeroUsuario, total;
+            string opcao, resultado;
+
+            Random aleatorio = new Random();
+            numeroAleatorio = aleatorio.Next(0, 10);
+
+            Console.WriteLine("Escolha (P)AR ou (I)MPAR");
+            opcao = Console.ReadLine();
+
+            Console.WriteLine("Informe um número de 0 à 10");
+            numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            total = numeroAleatorio + numeroUsuario;
+
+            if( total % 2 == 0)
+            {
+                Console.WriteLine("O número é par");
+                resultado = "P";
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar");
+                resultado = "I";
+            }
+
+            Console.WriteLine("A soma dos números é: " + total);
+
+            if (resultado == opcao)
+            {
+                Console.WriteLine("Você ganhou!");
+            }
+            else
+            {
+                Console.WriteLine("Você perdeu!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
