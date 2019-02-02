b8a8a1ce7718899358a2ea8d170c3c2adfe03dcb
Guto Xavier <gutoffline@gmail.com>
2019-02-01 22:27:44 -0200

    Calabouços e Dragões

--- 2019/Fevereiro/CalaboucosEDragoes/CalaboucosEDragoes/Program.cs
+++ 2019/Fevereiro/CalaboucosEDragoes/CalaboucosEDragoes/Program.cs
@@ -10,11 +10,14 @@ namespace CalaboucosEDragoes
     {
         static void Main(string[] args)
         {
-            string nome="", classe="", raca = "";
+            string nome="", classe="", raca = "", idade="";
             int vida = 100;
 
             Console.WriteLine("Informe seu nome aventureiro.");
             nome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua idade.");
+            idade = Console.ReadLine();
 
             Console.WriteLine("Informe sua raça.");
             raca = Console.ReadLine();
@@ -22,12 +25,7 @@ namespace CalaboucosEDragoes
             Console.WriteLine("Informe sua classe.");
             classe = Console.ReadLine();
 
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Ficha: ");
-            Console.WriteLine(nome);
-            Console.WriteLine(classe);
-            Console.WriteLine(raca);
-            Console.WriteLine("---------------------------------");
+            Ficha(nome , raca, classe , vida , idade);
 
             string resposta = "";
             Console.WriteLine("Deseja entrar na porta a sua direita?");
@@ -35,9 +33,65 @@ namespace CalaboucosEDragoes
 
             if(resposta == "sim")
             {
-
-
+                Console.WriteLine("Tem um monstrão aqui!!!");
+                Console.WriteLine("Ele mordeu sua orelha, o monstro chama MYKE TYSON!!!! \nPerca 50 de vida...");
+                vida = vida - 50;
             }
+
+            Ficha(nome , raca, classe, vida , idade);
+
+            Console.WriteLine("Deseja entrar na porta a sua direita?");
+            resposta = Console.ReadLine();
+
+            if (resposta == "sim")
+            {
+                Console.WriteLine("MAIS VIDA");
+                Console.WriteLine("GANHE 30 de vida...");
+                vida = vida + 30;
+            }
+
+            Console.WriteLine("Deseja entrar na porta a sua direita?");
+            resposta = Console.ReadLine();
+
+            if (resposta == "sim")
+            {
+                Console.WriteLine("Tem outro monstrão aqui!!!");
+                Console.WriteLine("Você tomou um cruzado!!!! \nPerca 50 de vida...");
+                vida = vida - 50;
+            }
+
+
+            if(vida <= 0)
+            {
+                Console.WriteLine("Morrrrreeeeeuuuuu!!!!");
+            }
+            else
+            {
+                Console.WriteLine("Deseja entrar na porta a sua direita?");
+                resposta = Console.ReadLine();
+
+                if (resposta == "sim")
+                {
+                    Console.WriteLine("AQUI TEM UM TESOURO aqui!!!");
+                    Console.WriteLine("VOCÊ ESTÁ MILIONÁRIO");
+                    vida = vida - 50;
+                }
+            }
+
+            Console.WriteLine("Fim de Jogo...até a próxima!");
+            Console.ReadKey();
+        }
+
+        static void Ficha(string nome , string classe, string raca, int vida, string idade)
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine("Ficha: ");
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Classe: " + classe);
+            Console.WriteLine("Raça: " + raca);
+            Console.WriteLine("Vida: " + vida);
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("===================================");
         }
     }
 }
