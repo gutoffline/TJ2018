8dd8271c90d85dcfd978c5cd4ea3efe30335ced4
Guto Xavier <gutoffline@gmail.com>
2019-02-06 22:28:20 -0200

    Hello World, Trocar Valores

---
+++ 2019/Fevereiro/Aula/TrocaValor/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TrocaValor
+{
+    partial class frmTrocaValor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btnTrocar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(13, 27);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(43, 13);
+            this.lblValor1.TabIndex = 0;
+            this.lblValor1.Text = "Valor 1:";
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Location = new System.Drawing.Point(131, 27);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(43, 13);
+            this.lblValor2.TabIndex = 1;
+            this.lblValor2.Text = "Valor 2:";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(54, 24);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(70, 20);
+            this.txtValor1.TabIndex = 2;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(172, 24);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(70, 20);
+            this.txtValor2.TabIndex = 3;
+            // 
+            // btnTrocar
+            // 
+            this.btnTrocar.Image = global::TrocaValor.Properties.Resources.iconfinder_Refresh_105225;
+            this.btnTrocar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrocar.Location = new System.Drawing.Point(87, 50);
+            this.btnTrocar.Name = "btnTrocar";
+            this.btnTrocar.Size = new System.Drawing.Size(75, 23);
+            this.btnTrocar.TabIndex = 4;
+            this.btnTrocar.Text = "TROCAR";
+            this.btnTrocar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTrocar.UseVisualStyleBackColor = true;
+            this.btnTrocar.Click += new System.EventHandler(this.btnTrocar_Click);
+            // 
+            // frmTrocaValor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 76);
+            this.Controls.Add(this.btnTrocar);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.lblValor1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmTrocaValor";
+            this.Text = "Troca o valor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btnTrocar;
+    }
+}
+
---
+++ 2019/Fevereiro/Aula/TrocaValor/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaValor
+{
+    public partial class frmTrocaValor : Form
+    {
+        public frmTrocaValor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrocar_Click(object sender, EventArgs e)
+        {
+            string auxiliar;
+            auxiliar = txtValor1.Text;
+            txtValor1.Text = txtValor2.Text;
+            txtValor2.Text = auxiliar;
+            MessageBox.Show("Troca efetuada com sucesso", "TROUCOU!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
