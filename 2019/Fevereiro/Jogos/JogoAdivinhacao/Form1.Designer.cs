9c408fb01c0a1501452b5cffefc2867320b63312
Guto Xavier <gutoffline@gmail.com>
2019-02-20 22:22:05 -0300

    Jogo Adivinhação

---
+++ 2019/Fevereiro/Jogos/JogoAdivinhacao/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace JogoAdivinhacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTentativas = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPlacar = new System.Windows.Forms.Button();
+            this.btnJogar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(91, 53);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tentativa:";
+            // 
+            // lblTentativas
+            // 
+            this.lblTentativas.AutoSize = true;
+            this.lblTentativas.Location = new System.Drawing.Point(149, 87);
+            this.lblTentativas.Name = "lblTentativas";
+            this.lblTentativas.Size = new System.Drawing.Size(13, 13);
+            this.lblTentativas.TabIndex = 4;
+            this.lblTentativas.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Valor:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(198, 53);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::JogoAdivinhacao.Properties.Resources.iconfinder_Dice_41429;
+            this.pictureBox1.Location = new System.Drawing.Point(-35, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnPlacar
+            // 
+            this.btnPlacar.Image = global::JogoAdivinhacao.Properties.Resources.iconfinder_019___Star_2792947;
+            this.btnPlacar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPlacar.Location = new System.Drawing.Point(197, 103);
+            this.btnPlacar.Name = "btnPlacar";
+            this.btnPlacar.Size = new System.Drawing.Size(101, 34);
+            this.btnPlacar.TabIndex = 7;
+            this.btnPlacar.Text = "Placar";
+            this.btnPlacar.UseVisualStyleBackColor = true;
+            this.btnPlacar.Click += new System.EventHandler(this.btnPlacar_Click);
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.Image = global::JogoAdivinhacao.Properties.Resources.iconfinder_package_games_board_1436;
+            this.btnJogar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnJogar.Location = new System.Drawing.Point(92, 103);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(99, 34);
+            this.btnJogar.TabIndex = 0;
+            this.btnJogar.Text = "Jogar";
+            this.btnJogar.UseVisualStyleBackColor = true;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 149);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnPlacar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTentativas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnJogar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Adivinhação";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTentativas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnPlacar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 2019/Fevereiro/Jogos/JogoAdivinhacao/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoAdivinhacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            int valorJogador, valorComputador;
+            Random dados = new Random();
+            valorComputador = dados.Next(0, 11);
+
+            valorJogador = Convert.ToInt32(txtValor.Text);
+
+            if(valorJogador == valorComputador)
+            {
+                MessageBox.Show("Você Ganhou!!!\nComputador: " + valorComputador.ToString(), "Parabéns", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GravarPlacar(" GANHOU");
+                Limpar();
+            }
+            else
+            {
+                MessageBox.Show("Você perdeu...\nComputador: " + valorComputador.ToString(), "Que pena", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                lblTentativas.Text = (Convert.ToInt32(lblTentativas.Text) + 1).ToString();
+            }
+
+            if(lblTentativas.Text == "3")
+            {
+                MessageBox.Show("Fim de Jogo", "Acabou", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GravarPlacar(" PERDEU");
+                Limpar();
+            }
+        }
+
+        private void Limpar()
+        {
+            txtNome.Text = "";
+            txtValor.Text = "";
+            lblTentativas.Text = "0";
+        }
+
+        private void GravarPlacar(string resultado)
+        {
+            StreamWriter arquivo;
+            string caminho = "C:\\teste\\placar.txt";
+            arquivo = File.AppendText(caminho);
+
+            arquivo.Write(txtNome.Text);
+            arquivo.WriteLine(resultado);
+            arquivo.WriteLine();
+
+            arquivo.Close();
+        }
+
+        private void btnPlacar_Click(object sender, EventArgs e)
+        {
+            string caminho = "C:\\teste\\placar.txt";
+            System.Diagnostics.Process.Start("notepad", caminho);
+        }
+    }
+}
