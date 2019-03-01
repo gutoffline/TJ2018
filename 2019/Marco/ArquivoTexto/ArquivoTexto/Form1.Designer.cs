7703197702059f66d87e19744b77bfc360f3b650
Guto Xavier <gutoffline@gmail.com>
2019-03-01 19:35:49 -0300

    Arquivo de Texto

---
+++ 2019/Marco/ArquivoTexto/ArquivoTexto/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ArquivoTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(49, 50);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(145, 26);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Image = global::ArquivoTexto.Properties.Resources.iconfinder_icon_55_document_text_315178;
+            this.btnAbrir.Location = new System.Drawing.Point(49, 82);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(221, 77);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Image = global::ArquivoTexto.Properties.Resources.iconfinder_floppy_285657;
+            this.btnGravar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGravar.Location = new System.Drawing.Point(198, 49);
+            this.btnGravar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(72, 28);
+            this.btnGravar.TabIndex = 0;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 187);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnGravar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Criar arquivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
+
---
+++ 2019/Marco/ArquivoTexto/ArquivoTexto/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArquivoTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            StreamWriter arquivo;
+            string CaminhoNome = "C:\\teste\\registros.txt";
+
+            //utilizando o metodo para criar um arquivo texto
+            //e associando o caminho e nome ao metodo
+            arquivo = File.AppendText(CaminhoNome);
+
+            //aqui, exemplo de escrever no arquivo texto
+            //escrevendo o titulo
+            arquivo.Write("Nome: ");
+            arquivo.Write(txtNome.Text);
+            //pulando linha sem escrita
+            arquivo.WriteLine();
+
+            //fechando o arquivo texto com o método .Close()
+            arquivo.Close();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            //Colocando o endereço físico (caminho do arquivo texto) 
+            string Caminho = "C:\\teste\\registros.txt";
+
+            //Abrindo o arquivo texto em um leitor de texto, no caso, 
+            //o bloco de notas(notepad)
+
+            System.Diagnostics.Process.Start("notepad", Caminho);
+            //feito isso, será aperto o bloco de notas com o conteúdo do 
+            //registros.txt
+        }
+    }
+}
